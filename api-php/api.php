7b38385867e39e67f997d1b31dcb9b414cb76511
Victor Gabriel Bessemer Santos <161629490+Victorbessemer2@users.noreply.github.com>
2026-04-07 16:13:06 -0300

    Add files via upload

--- api-php/api.php
+++ api-php/api.php
@@ -2,15 +2,32 @@
     //cabecalho
     header("Content-Type: application/json");
 
+    $metodo = $_SERVER['REQUEST_METHOD'];
+
+    //echo "metodo de requisicao: ". $metodo
+
+    switch ($metodo) {
+        case 'GET';
+            echo "aqui acoes do metodo get";
+            break;
+        case 'POST';
+            echo "aqui acoes do metodo post";
+            break;
+        default:
+            echo "metodo nao encontrado";
+            break;
+
+    }
+
 
     //conteudo
-    $usuarios = [
-        ["id" =>1, "nome" => "Maria", "email" => "maria@gmail.com"],
-        ["id" =>2, "nome" => "Joao", "email" => "joao@gmail.com"]
-    ];
+    // $usuarios = [
+    //     ["id" =>1, "nome" => "Maria", "email" => "maria@gmail.com"],
+    //     ["id" =>2, "nome" => "Joao", "email" => "joao@gmail.com"]
+    // ];
 
-    //converte para json e retorna
-    echo json_encode($usuarios);
+    // //converte para json e retorna
+    // echo json_encode($usuarios);
 
 
 ?>
